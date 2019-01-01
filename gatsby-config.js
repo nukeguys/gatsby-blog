519f17a85871d407eac2dbc57a887c8560af9c80
lucas-sung <lucas@kakaopage.com>
2019-01-01 21:51:08 +0900

    기본정보 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `nukeguys blog`,
+    author: `nukeguys`,
+    description: `about development, life, something...`,
+    siteUrl: `https://nukeguys.github.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: ``,
     },
   },
   plugins: [
