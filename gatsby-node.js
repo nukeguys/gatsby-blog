12808b05d733854d68871a03c7a21bbccc51f627
lucas-sung <lucas@kakaopage.com>
2019-01-01 14:25:48 +0900

    module resolve path 'src'추가
    https://www.gatsbyjs.org/docs/add-custom-webpack-config/#absolute-imports

--- gatsby-node.js
+++ gatsby-node.js
@@ -68,3 +68,11 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     })
   }
 }
+
+exports.onCreateWebpackConfig = ({ stage, actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      modules: [path.resolve(__dirname, `src`), `node_modules`],
+    },
+  })
+}
